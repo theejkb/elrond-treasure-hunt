134544fe07346216ff3f071dd09427f5d7dfb6f1
Thomas Bocquez <tbocquez@steamulo.com>
2022-08-09 14:14:24 +0200

    chore: Add default meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
