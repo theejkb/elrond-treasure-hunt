64a2c0e6b21227264e6d1b94384b07d379ea0c0d
Thomas Bocquez <tbocquez@steamulo.com>
2022-11-27 23:20:26 +0100

    feat: add bcrypt for words

--- cases.ts
+++ cases.ts
@@ -1,174 +1,209 @@
 import { Case } from './types'
 
+// 11 est l'index du mois => Décembre
 export const cases: Case[] = [
   {
-    project: 'Dragons Arena',
+    project: 'Dragons Arena', // 1
     link: 'https://discord.gg/h6T9vp3J86',
-    encryptedWord: 'Jade38',
-    encryptedWalletWord: 'tournament',
-    availableDate: new Date('2022-09-27T15:00:00'),
+    imgUrl: 'dragons_arena.png',
+    encryptedWord: '$2a$10$uPgzV15gmqBDw5.uOIdWpOSFauGBtCCcv.uv1BA8xv54ad3wHNc6e', // Agate48
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 1),
+    hint: '🎵',
   },
   {
-    project: 'PLATA',
-    link: 'https://twitter.com/plataNetwork',
-    imgUrl: '/project_logos/plata.jpg',
-    encryptedWord: 'topaz87',
-    encryptedWalletWord: 'poker',
-    availableDate: new Date('2022-09-28T15:00:00'),
+    project: 'Battle of nodes', // 2
+    link: 'https://twitter.com/BattleOfNodeNFT',
+    imgUrl: 'battle_of_nodes.png',
+    encryptedWord: '$2a$10$uPgzV15gmqBDw5.uOIdWpO/wnNdsCnomPcxHIf4KkEBXBZqPhf9Mi', // Aragonite05
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 2),
+    hint: 'A new player has entered the game',
   },
   {
-    project: 'Elrond Mafia',
+    project: 'VSWatch', // 3
+    link: 'https://vswatch.club/',
+    imgUrl: 'vswatch.png',
+    encryptedWord: '$2a$10$uPgzV15gmqBDw5.uOIdWpOoDnTWznpF6WgSWxS/JjVQzqD7BcFgVW', // Spinel02
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 3),
+    hint: '3 letters in the dark',
+  },
+  {
+    project: 'Demiourgos', // 4
+    link: 'https://twitter.com/DemiourgosH',
+    imgUrl: 'demiourgos.png',
+    encryptedWord: '$2a$10$uPgzV15gmqBDw5.uOIdWpOgNujtQiUeaQZ4aD6A.eFo/L4NROQEQK', // Labradorite12
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 4),
+    hint: 'Where’s is the Lost Ark ? ',
+  },
+  {
+    project: 'Angry Penguins', // 5
+    link: 'https://discord.gg/hfK6CvgF8M',
+    imgUrl: 'angry_penguins.png',
+    encryptedWord: '$2a$10$uPgzV15gmqBDw5.uOIdWpOJA7UIeJYbq5xKs2t34IDiUCBoUW89ki', // Aventurine22
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 5),
+    hint: 'Ask a question after 7pm'
+  },
+  {
+    project: 'Calileo', // 6
+    link: 'https://calileo.co/ethos',
+    imgUrl: 'calileo.png',
+    encryptedWord: '$2a$10$uPgzV15gmqBDw5.uOIdWpOMYEpFY95LXrNssk5m9xOPXvXWPgTNYu', // Amethyst09
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 6),
+    hint: 'Touch the invisible'
+  },
+  {
+    project: 'Elrond Mafia', // 7
+    link: 'https://discord.gg/elrondmafia',
+    imgUrl: 'elrond_mafia.png',
+    encryptedWord: '$2a$10$uPgzV15gmqBDw5.uOIdWpOkW.1y4zQ2TVF6PnDENjp.hcm6kBrIjC', // Topaz58
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 7),
+    hint: 'Ask yourself the right questions. The mafia does not forgive'
+  },
+  {
+    project: 'CryptoPittz', // 8
+    link: 'https://discord.gg/cryptopittz',
+    imgUrl: 'crypto_pittz.png',
+    encryptedWord: '$2a$10$uPgzV15gmqBDw5.uOIdWpOhzcc.fQ799J5U9beiCiQCkaNVsfAERi', // Emerald20
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 8),
+    hint: 'We need to talk'
+  },
+  {
+    project: 'Founding 8', // 9
+    link: 'https://discord.gg/tC4us5eJ43',
+    imgUrl: 'founding_8.png',
+    encryptedWord: '$2a$10$uPgzV15gmqBDw5.uOIdWpOPHjaJiFvwnTFlLRt95jx6zCfBbNNyU2', // Carnelian11
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 9),
+    hint: '1Beauty 3lies in the 5eyes of the beholder'
+  },
+  {
+    project: 'Gaupa Labs', // 10
+    link: 'https://twitter.com/GaupaLabs',
+    imgUrl: 'gaupa_labs.png',
+    encryptedWord: '$2a$10$uPgzV15gmqBDw5.uOIdWpOCe/rv0gyIEWG06yo2xPI0IZ87T1.Nuq', // Tanzanite03
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 10),
+    hint: 'The X marks the spot'
+  },
+  {
+    project: 'HODLcards', // 11
+    link: 'https://dsc.gg/HODLcards',
+    imgUrl: 'hodlcards.png',
+    encryptedWord: '$2a$10$uPgzV15gmqBDw5.uOIdWpOHuVkD2GzUsHJmb5s0jWeeMc4x6086gS', // Gold12
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 11),
+    hint: 'Get a tattoo and cash-in !'
+  },
+  {
+    project: 'JCorp', // 12
+    link: 'https://play.jcorpbattleofgods.com/',
+    imgUrl: 'jcorp.png',
+    encryptedWord: '$2a$10$uPgzV15gmqBDw5.uOIdWpOpTp/jrEB58Xq3kWTYm414WNGwOaCWAS', // Kyanite99
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 12),
+    hint: 'What’s next ?'
+  },
+  {
+    project: 'Monkeys Supremacy', // 13
     link: 'https://discord.gg/jZZRrD8Qbz',
     imgUrl: '/project_logos/elrond_mafia.webp',
     encryptedWord: 'emerald29',
-    encryptedWalletWord: 'flop',
-    availableDate: new Date('2022-09-29T15:00:00'),
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 13),
   },
   {
-    project: 'Elrond Mafia',
+    project: 'PLATA', // 14
     link: 'https://discord.gg/jZZRrD8Qbz',
+    imgUrl: '/project_logos/elrond_mafia.webp',
     encryptedWord: 'emerald29',
-    encryptedWalletWord: 'flop',
-    availableDate: new Date('2022-09-29T15:00:00'),
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 14),
   },
   {
-    project: 'Elrond Mafia',
+    project: 'TOC', // 15
     link: 'https://discord.gg/jZZRrD8Qbz',
+    imgUrl: '/project_logos/elrond_mafia.webp',
     encryptedWord: 'emerald29',
-    encryptedWalletWord: 'flop',
-    availableDate: new Date('2022-09-29T15:00:00'),
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 15),
   },
   {
-    project: 'Elrond Mafia',
+    project: 'Lucky1000', // 16
     link: 'https://discord.gg/jZZRrD8Qbz',
+    imgUrl: '/project_logos/elrond_mafia.webp',
     encryptedWord: 'emerald29',
-    encryptedWalletWord: 'flop',
-    availableDate: new Date('2022-09-29T15:00:00'),
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 16),
   },
   {
-    project: 'Elrond Mafia',
+    project: 'Wwwatch', // 17
     link: 'https://discord.gg/jZZRrD8Qbz',
+    imgUrl: '/project_logos/elrond_mafia.webp',
     encryptedWord: 'emerald29',
-    encryptedWalletWord: 'flop',
-    availableDate: new Date('2022-09-29T15:00:00'),
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 17),
   },
   {
-    project: 'Elrond Mafia',
+    project: 'Frame it', // 18
     link: 'https://discord.gg/jZZRrD8Qbz',
+    imgUrl: '/project_logos/elrond_mafia.webp',
     encryptedWord: 'emerald29',
-    encryptedWalletWord: 'flop',
-    availableDate: new Date('2022-09-29T15:00:00'),
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 18),
   },
   {
-    project: 'Elrond Mafia',
+    project: 'QoWatt', // 19
     link: 'https://discord.gg/jZZRrD8Qbz',
+    imgUrl: '/project_logos/elrond_mafia.webp',
     encryptedWord: 'emerald29',
-    encryptedWalletWord: 'flop',
-    availableDate: new Date('2022-09-29T15:00:00'),
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 19),
   },
   {
-    project: 'Elrond Mafia',
+    project: 'Dinovox', // 20
     link: 'https://discord.gg/jZZRrD8Qbz',
+    imgUrl: '/project_logos/elrond_mafia.webp',
     encryptedWord: 'emerald29',
-    encryptedWalletWord: 'flop',
-    availableDate: new Date('2022-09-29T15:00:00'),
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 20),
   },
   {
-    project: 'Elrond Mafia',
+    project: 'Drifters', // 21
     link: 'https://discord.gg/jZZRrD8Qbz',
+    imgUrl: '/project_logos/elrond_mafia.webp',
     encryptedWord: 'emerald29',
-    encryptedWalletWord: 'flop',
-    availableDate: new Date('2022-09-29T15:00:00'),
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 21),
   },
   {
-    project: 'Elrond Mafia',
+    project: 'Fightoons', // 22
     link: 'https://discord.gg/jZZRrD8Qbz',
+    imgUrl: '/project_logos/elrond_mafia.webp',
     encryptedWord: 'emerald29',
-    encryptedWalletWord: 'flop',
-    availableDate: new Date('2022-09-29T15:00:00'),
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 22),
   },
   {
-    project: 'Elrond Mafia',
+    project: 'Globees', // 23
     link: 'https://discord.gg/jZZRrD8Qbz',
+    imgUrl: '/project_logos/elrond_mafia.webp',
     encryptedWord: 'emerald29',
-    encryptedWalletWord: 'flop',
-    availableDate: new Date('2022-09-29T15:00:00'),
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 23),
   },
   {
-    project: 'Elrond Mafia',
+    project: 'MultiversX France',
     link: 'https://discord.gg/jZZRrD8Qbz',
+    imgUrl: '/project_logos/elrond_mafia.webp',
     encryptedWord: 'emerald29',
-    encryptedWalletWord: 'flop',
-    availableDate: new Date('2022-09-29T15:00:00'),
-  },
-  {
-    project: 'Elrond Mafia',
-    link: 'https://discord.gg/jZZRrD8Qbz',
-    encryptedWord: 'emerald29',
-    encryptedWalletWord: 'flop',
-    availableDate: new Date('2022-09-29T15:00:00'),
-  },
-  {
-    project: 'Elrond Mafia',
-    link: 'https://discord.gg/jZZRrD8Qbz',
-    encryptedWord: 'emerald29',
-    encryptedWalletWord: 'flop',
-    availableDate: new Date('2022-09-29T15:00:00'),
-  },
-  {
-    project: 'Elrond Mafia',
-    link: 'https://discord.gg/jZZRrD8Qbz',
-    encryptedWord: 'emerald29',
-    encryptedWalletWord: 'flop',
-    availableDate: new Date('2022-09-29T15:00:00'),
-  },
-  {
-    project: 'Elrond Mafia',
-    link: 'https://discord.gg/jZZRrD8Qbz',
-    encryptedWord: 'emerald29',
-    encryptedWalletWord: 'flop',
-    availableDate: new Date('2022-09-29T15:00:00'),
-  },
-  {
-    project: 'Elrond Mafia',
-    link: 'https://discord.gg/jZZRrD8Qbz',
-    encryptedWord: 'emerald29',
-    encryptedWalletWord: 'flop',
-    availableDate: new Date('2022-09-29T15:00:00'),
-  },
-  {
-    project: 'Elrond Mafia',
-    link: 'https://discord.gg/jZZRrD8Qbz',
-    encryptedWord: 'emerald29',
-    encryptedWalletWord: 'flop',
-    availableDate: new Date('2022-09-29T15:00:00'),
-  },
-  {
-    project: 'Elrond Mafia',
-    link: 'https://discord.gg/jZZRrD8Qbz',
-    encryptedWord: 'emerald29',
-    encryptedWalletWord: 'flop',
-    availableDate: new Date('2022-09-29T15:00:00'),
-  },
-  {
-    project: 'Elrond Mafia',
-    link: 'https://discord.gg/jZZRrD8Qbz',
-    encryptedWord: 'emerald29',
-    encryptedWalletWord: 'flop',
-    availableDate: new Date('2022-09-29T15:00:00'),
-  },
-  {
-    project: 'Elrond Mafia',
-    link: 'https://discord.gg/jZZRrD8Qbz',
-    encryptedWord: 'emerald29',
-    encryptedWalletWord: 'flop',
-    availableDate: new Date('2022-09-29T15:00:00'),
-  },
-  {
-    project: 'Elrond Mafia',
-    link: 'https://discord.gg/jZZRrD8Qbz',
-    encryptedWord: 'emerald29',
-    encryptedWalletWord: 'flop',
-    availableDate: new Date('2022-09-29T15:00:00'),
+    encryptedWalletWord: 'fill',
+    availableDate: new Date(2022, 11, 24),
   },
 ]
---
+++ pages/api/cases.tsx
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { cases } from '../../cases'
+import { Case } from '../../types'
+import bcrypt from 'bcryptjs'
+
+export default function handler(
+  _req: NextApiRequest,
+  res: NextApiResponse<Case[]>,
+) {
+  // const test: Case[] = cases.map((_case) => {
+  //   return {
+  //     ..._case,
+  //     encryptedWord: bcrypt.hashSync(
+  //       _case.encryptedWord,
+  //       '$2a$10$uPgzV15gmqBDw5.uOIdWpO',
+  //     ),
+  //   }
+  // })
+  // console.log(test)
+
+  res
+    .status(200)
+    .json(cases.filter((_case) => _case.availableDate <= new Date()))
+}
